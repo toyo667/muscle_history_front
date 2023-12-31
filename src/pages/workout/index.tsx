ad8634aa9dde576f71fe6bd392ba161361ec0786
Hiro <poyo2c@gmail.com>
2023-12-31 20:23:06 +0900

    Conditionの選択実装と、UIをMUI二変更

--- src/pages/workout/index.tsx
+++ src/pages/workout/index.tsx
@@ -3,11 +3,20 @@ import { RequiredAuth } from "../../utils/requiredAuth";
 import { api } from "../../utils/apis";
 import { WorkoutSession, WorkoutSessionApiFactory } from "../../openapi";
 import { useMasters } from "../../hooks/useMasters";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
 
 const Workout = () => {
   const [activeSession, setActiveSession] = useState<WorkoutSession>();
   const { masterData } = useMasters();
   const [loading, setLoading] = useState(false);
+  const [condition, setCondition] = useState("");
 
   const initialize = useRef(false);
 
@@ -33,11 +42,12 @@ const Workout = () => {
   }, [updateWorkoutSession]);
 
   const startSession = useCallback(async () => {
+    if (!condition) return;
     await api(WorkoutSessionApiFactory).v1WorkoutSessionCreate({
-      condition: masterData?.conditions[0].id,
+      condition: condition,
     } as any);
     updateWorkoutSession();
-  }, [masterData?.conditions, updateWorkoutSession]);
+  }, [condition, updateWorkoutSession]);
 
   const endSession = useCallback(async () => {
     const id = activeSession?.id;
@@ -55,18 +65,38 @@ const Workout = () => {
       ) : (
         <div>
           {activeSession ? (
+            // セッションあり
             <div>
               <h2>session: {activeSession.id} is currentry actived.</h2>
               <div>
-                <button onClick={endSession}>endsession</button>
+                <Button onClick={endSession}>endsession</Button>
               </div>
             </div>
           ) : (
+            // セッションなし
             <div>
               <h2>no active session</h2>
               <div>
                 <h3>startSession</h3>
-                <button onClick={startSession}>start</button>
+                <Box sx={{ minWidth: 120 }}>
+                  <FormControl fullWidth>
+                    <InputLabel id="cond-select">Condition</InputLabel>
+                    <Select
+                      labelId="cond-select"
+                      value={condition}
+                      onChange={(e) => {
+                        setCondition(e.target.value);
+                      }}
+                    >
+                      {masterData?.conditions.map((c) => (
+                        <MenuItem value={c.id} key={c.id}>
+                          {c.feel}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Box>
+                <Button onClick={startSession}>start</Button>
               </div>
             </div>
           )}

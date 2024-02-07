f82c3f49d9c6764248b2901345dba0c98df6a4e1
Hiro <poyo2c@gmail.com>
2024-02-07 20:18:24 +0900

    デプロイ用パス変更

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yarn build
 CURRENT=$(cd $(dirname $0);pwd)
-rsync -arv --delete -e "ssh -i ~/.ssh/aws-wsl.pem" ${CURRENT}/build aws:~/muscle_history_front/
+rsync -arv --delete -e "ssh -i ~/.ssh/id_rsa" ${CURRENT}/build svps:~/Git/muscle_history_front/
